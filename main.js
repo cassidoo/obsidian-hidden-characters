555e670c2e9e04a0f403b44a8d5624cd69096f3f
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2023-03-17 23:11:36 -0500

    Add Settings and base plugin features

--- main.js
+++ main.js
@@ -1,49 +1,91 @@
-import { Plugin, PluginSettingTab, App, Setting } from "obsidian";
+import { Plugin, PluginSettingTab, App, Setting, MarkdownView } from "obsidian";
+
+const LEFT_TO_RIGHT_OVERRIDE = "\u202d";
+const POP_DIRECTIONAL_FORMATTING = "\u202c";
+
+const DEFAULT_SETTINGS = {
+	enabled: true,
+	backgroundColor: "rgba(255, 0, 0, 0.3)",
+	textColor: "black",
+};
 
 export default class HiddenCharactersPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon("dice", "Sample Plugin", (evt) => {
-			// type MouseEvent
-			// Called when the user clicks the icon.
-			new Notice("This is a notice!");
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText("Status Bar Text");
-		// num hidden characters?
-
-		// This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand({
-		// 	id: "sample-editor-command",
-		// 	name: "Sample editor command",
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection("Sample Editor Command");
-		// 	},
-		// });
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new HiddenCharactersSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt) => {
-			console.log("click", evt);
+		this.registerCodeMirror((cm) => {
+			cm.setOption("specialChars", /[\u202d\u202c]/);
+			cm.setOption(
+				"specialCharPlaceholder",
+				this.specialCharPlaceholder.bind(this)
+			);
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		this.statusBar = this.addStatusBarItem();
 		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+			window.setInterval(() => this.updateStatusBar(), 1000)
 		);
+
+		this.addSettingTab(new HiddenCharactersSettingTab(this.app, this));
 	}
 
-	onunload() {}
+	refresh() {
+		if (this.settings.enabled) {
+			this.registerCodeMirror((cm) => {
+				cm.setOption("specialChars", /[\u202d\u202c]/);
+				cm.setOption(
+					"specialCharPlaceholder",
+					this.specialCharPlaceholder.bind(this)
+				);
+			});
+		} else {
+			this.app.workspace.iterateCodeMirrors((cm) => {
+				cm.setOption("specialChars", null);
+				cm.setOption("specialCharPlaceholder", null);
+			});
+		}
+	}
+
+	specialCharPlaceholder(ch) {
+		const span = document.createElement("span");
+		span.textContent = ch;
+		span.title = this.getCharacterName(ch);
+		span.classList.add("hidden-unicode-character");
+		span.style.backgroundColor = this.settings.backgroundColor;
+		span.style.color = this.settings.textColor;
+		return span;
+	}
+
+	getCharacterName(ch) {
+		switch (ch) {
+			case LEFT_TO_RIGHT_OVERRIDE:
+				return "Left-to-right override (U+202D)";
+			case POP_DIRECTIONAL_FORMATTING:
+				return "Pop directional formatting (U+202C)";
+			default:
+				return `Unicode character: ${ch}`;
+		}
+	}
+
+	updateStatusBar() {
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (!view) {
+			this.statusBar.setText("");
+			return;
+		}
+
+		const content = view.file.content;
+		const hiddenUnicodeCount = (content.match(/[\u202d\u202c]/g) || []).length;
+		this.statusBar.setText(`Hidden Unicode Characters: ${hiddenUnicodeCount}`);
+	}
+
+	onunload() {
+		this.app.workspace.iterateCodeMirrors((cm) => {
+			cm.setOption("specialChars", null);
+			cm.setOption("specialCharPlaceholder", null);
+		});
+		this.statusBar.remove();
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -59,24 +101,69 @@ class HiddenCharactersSettingTab extends PluginSettingTab {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
+
+	display() {
+		const { containerEl } = this;
+
+		containerEl.empty();
+		containerEl.createEl("h2", { text: "Show Hidden Unicode Settings" });
+
+		new Setting(containerEl)
+			.setName("Enable")
+			.setDesc("Toggle showing hidden Unicode characters")
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.enabled)
+					.onChange(async (value) => {
+						this.plugin.settings.enabled = value;
+						await this.plugin.saveSettings();
+						this.plugin.refresh();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Background Color")
+			.setDesc("Set the background color for hidden Unicode characters")
+			.addText((text) =>
+				text
+					.setValue(this.plugin.settings.backgroundColor)
+					.onChange(async (value) => {
+						this.plugin.settings.backgroundColor = value;
+						await this.plugin.saveSettings();
+						this.plugin.refresh();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Text Color")
+			.setDesc("Set the text color for hidden Unicode characters")
+			.addText((text) =>
+				text
+					.setValue(this.plugin.settings.textColor)
+					.onChange(async (value) => {
+						this.plugin.settings.textColor = value;
+						await this.plugin.saveSettings();
+						this.plugin.refresh();
+					})
+			);
+	}
 }
 
 // TODO: Functions to write
-// - One to show hidden characters
 // - One to remove hidden characters in a file
 
 // This renames files that have hidden characters in the name
-let count = 0;
-await app.fileManager.runAsyncLinkUpdate(async () => {
-	for (let file of app.vault.getMarkdownFiles()) {
-		let newName = file.name.replace(/[\u202d\u202c]/g, "");
-		if (newName !== file.name) {
-			count++;
-			await app.vault.rename(
-				file,
-				file.path.substring(0, file.path.length - file.name.length) + newName
-			);
-		}
-	}
-});
-console.log("Renamed " + count + " files.");
+// let count = 0;
+// await app.fileManager.runAsyncLinkUpdate(async () => {
+// 	for (let file of app.vault.getMarkdownFiles()) {
+// 		let newName = file.name.replace(/[\u202d\u202c]/g, "");
+// 		if (newName !== file.name) {
+// 			count++;
+// 			await app.vault.rename(
+// 				file,
+// 				file.path.substring(0, file.path.length - file.name.length) + newName
+// 			);
+// 		}
+// 	}
+// });
+// console.log("Renamed " + count + " files.");
